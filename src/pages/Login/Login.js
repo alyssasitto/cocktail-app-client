c1eb585d003e24d3352108be617103d424bd4ecf
Alyssa Sitto <alyssasitto1@gmail.com>
2023-02-15 22:13:48 -0800

    Add images and styling for login and signup pages

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,8 @@
 import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import { AuthContext } from "../../context/auth.context";
+import { ThemeContext } from "../../context/theme.context";
 import axios from "axios";
 
 require("../Form.css");
@@ -9,9 +11,11 @@ export const Login = () => {
 	const API_URL = process.env.REACT_APP_API_URL;
 
 	const { storeItems } = useContext(AuthContext);
+	const { theme } = useContext(ThemeContext);
 
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [inputType, setInputType] = useState("password");
 	const [errMessage, setErrMessage] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -23,67 +27,141 @@ export const Login = () => {
 		setPassword(e.target.value);
 	};
 
+	// Function for showing and hiding the password
+	const toggleInputType = () => {
+		if (inputType === "password") {
+			setInputType("text");
+		} else {
+			setInputType("password");
+		}
+	};
+
 	// Function for when a user signs in
 	const handleSubmit = (e) => {
 		// Prevent the page from reloading
 		e.preventDefault();
 
-		// Set loading state to true
-		setIsLoading(true);
+		if (email === "" || password === "") {
+			setErrMessage("Please enter an email and password");
+		} else {
+			// Set loading state to true
+			setIsLoading(true);
 
-		const body = {
-			email,
-			password,
-		};
+			const body = {
+				email,
+				password,
+			};
 
-		// Make request to login route and send body with email and password that user typed in
-		axios
-			.post(`${API_URL}/login`, body)
-			.then((response) => {
-				// Use the storeItems() function to store the authToken and refreshToken
-				storeItems(response.data.authToken, response.data.refreshToken);
-				// Reload the page
-				window.location.reload();
-			})
-			.catch((err) => {
-				setIsLoading(false);
-				setErrMessage(err.response.data.err);
-			});
+			// Make request to login route and send body with email and password that user typed in
+			axios
+				.post(`${API_URL}/login`, body)
+				.then((response) => {
+					// Use the storeItems() function to store the authToken and refreshToken
+					storeItems(response.data.authToken, response.data.refreshToken);
+					// Reload the page
+					window.location.reload();
+				})
+				.catch((err) => {
+					setIsLoading(false);
+					setErrMessage(err.response.data.err);
+				});
+		}
 	};
 
 	return (
 		<div className="form-page">
 			<Navbar />
 
-			<div className="form-content">
-				{isLoading && <p>loading...</p>}
+			{isLoading && <p>loading...</p>}
 
-				<form onSubmit={handleSubmit} className="form">
-					<div>
-						<label htmlFor="email">Email:</label>
-						<input
-							type="text"
-							name="email"
-							value={email}
-							onChange={handleEmail}
-						/>
-					</div>
-					<div>
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							name="password"
-							value={password}
-							onChange={handlePassword}
-						/>
-					</div>
-					<button type="submit">submit</button>
-				</form>
+			{!isLoading && (
+				<div className="form-content">
+					<h1>Login</h1>
 
-				{errMessage && <p>{errMessage}</p>}
+					<form onSubmit={handleSubmit} className="form">
+						<div>
+							<label htmlFor="email">Email</label>
+							<div className="input-container">
+								<input
+									type="text"
+									name="email"
+									value={email}
+									onChange={handleEmail}
+								/>
+								{theme === "" ? (
+									<img
+										src="images/envelope-black.svg"
+										alt="Envelope icon"
+									></img>
+								) : (
+									<img
+										src="images/envelope-white.svg"
+										alt="Envelope icon"
+									></img>
+								)}
+							</div>
+						</div>
+						<div>
+							<label htmlFor="password">Password</label>
+							<div className="input-container">
+								<input
+									type={inputType}
+									name="password"
+									value={password}
+									onChange={handlePassword}
+								/>
+								{theme === "" ? (
+									<img src="images/lock-black.svg" alt="Lock icon"></img>
+								) : (
+									<img src="images/lock-white.svg" alt="Lock icon"></img>
+								)}
 
-				<a href="/">Home</a>
-			</div>
+								<button type="button" onClick={() => toggleInputType()}>
+									{inputType === "password" &&
+										(theme === "" ? (
+											<img
+												src="images/eye-black.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										) : (
+											<img
+												src="images/eye-white.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										))}
+
+									{inputType === "text" &&
+										(theme === "" ? (
+											<img
+												src="images/eye-slash-black.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										) : (
+											<img
+												src="images/eye-slash-white.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										))}
+								</button>
+							</div>
+						</div>
+
+						{errMessage && <p className="err-message">{errMessage}</p>}
+
+						<button type="submit" className="form-btn">
+							Login
+						</button>
+					</form>
+
+					<Link to="/signup" className="link">
+						Don't have an account yet? <span>Signup here</span>
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { ThemeContext } from "../../context/theme.context";
 import Navbar from "../../components/Navbar/Navbar";
 import axios from "axios";
 
@@ -10,9 +11,12 @@ export const Signup = () => {
 
 	const navigate = useNavigate();
 
+	const { theme } = useContext(ThemeContext);
+
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [inputType, setInputType] = useState("password");
 	const [isLoading, setIsLoading] = useState(false);
 	const [errMessage, setErrMessage] = useState("");
 
@@ -28,63 +32,153 @@ export const Signup = () => {
 		setPassword(e.target.value);
 	};
 
+	// Function for showing and hiding the password
+	const toggleInputType = () => {
+		if (inputType === "password") {
+			setInputType("text");
+		} else {
+			setInputType("password");
+		}
+	};
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		setIsLoading(true);
+		if (name === "" || email === "" || password === "") {
+			setErrMessage("Please enter a name, email, and password");
+		} else {
+			setIsLoading(true);
 
-		const body = {
-			name,
-			email,
-			password,
-		};
+			const body = {
+				name,
+				email,
+				password,
+			};
 
-		axios
-			.post(`${API_URL}/signup`, body)
-			.then(() => {
-				navigate("/profile");
-			})
-			.catch((err) => {
-				console.log(err.response.data.err);
-				setErrMessage(err.response.data.err);
-			});
+			axios
+				.post(`${API_URL}/signup`, body)
+				.then(() => {
+					navigate("/profile");
+				})
+				.catch((err) => {
+					setIsLoading(false);
+					console.log(err.response.data.err);
+					setErrMessage(err.response.data.err);
+				});
+		}
 	};
 
 	return (
 		<div className="form-page">
 			<Navbar />
 
-			<div className="form-content">
-				<form onSubmit={handleSubmit} className="form">
-					<div>
-						<label htmlFor="name">Name:</label>
-						<input type="text" name="name" value={name} onChange={handleName} />
-					</div>
-					<div>
-						<label htmlFor="email">Email:</label>
-						<input
-							type="text"
-							name="email"
-							value={email}
-							onChange={handleEmail}
-						/>
-					</div>
-					<div>
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							name="password"
-							value={password}
-							onChange={handlePassword}
-						/>
-					</div>
-					<button type="submit">submit</button>
-				</form>
+			{isLoading && <p>loading...</p>}
 
-				{errMessage && <p>{errMessage}</p>}
+			{!isLoading && (
+				<div className="form-content">
+					<h1>Signup</h1>
+					<form onSubmit={handleSubmit} className="form">
+						<div>
+							<label htmlFor="name">Name</label>
+							<div className="input-container">
+								<input
+									type="text"
+									name="name"
+									value={name}
+									onChange={handleName}
+								/>
 
-				<a href="/">Home</a>
-			</div>
+								{theme === "" ? (
+									<img src="images/user-black.svg" alt="User icon"></img>
+								) : (
+									<img src="images/user-white.svg" alt="User icon"></img>
+								)}
+							</div>
+						</div>
+						<div>
+							<label htmlFor="email">Email</label>
+							<div className="input-container">
+								<input
+									type="text"
+									name="email"
+									value={email}
+									onChange={handleEmail}
+								/>
+
+								{theme === "" ? (
+									<img
+										src="images/envelope-black.svg"
+										alt="Envelope icon"
+									></img>
+								) : (
+									<img
+										src="images/envelope-white.svg"
+										alt="Envelope icon"
+									></img>
+								)}
+							</div>
+						</div>
+						<div>
+							<label htmlFor="password">Password</label>
+							<div className="input-container">
+								<input
+									type={inputType}
+									name="password"
+									value={password}
+									onChange={handlePassword}
+								/>
+								{theme === "" ? (
+									<img src="images/lock-black.svg" alt="Lock icon"></img>
+								) : (
+									<img src="images/lock-white.svg" alt="Lock icon"></img>
+								)}
+
+								<button type="button" onClick={() => toggleInputType()}>
+									{inputType === "password" &&
+										(theme === "" ? (
+											<img
+												src="images/eye-black.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										) : (
+											<img
+												src="images/eye-white.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										))}
+
+									{inputType === "text" &&
+										(theme === "" ? (
+											<img
+												src="images/eye-slash-black.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										) : (
+											<img
+												src="images/eye-slash-white.svg"
+												className="eye-icon"
+												alt="Eye icon"
+											></img>
+										))}
+								</button>
+							</div>
+						</div>
+
+						{errMessage && <p className="err-message">{errMessage}</p>}
+
+						<button type="submit" className="form-btn">
+							Signup
+						</button>
+					</form>
+
+					<Link to="/login" className="link">
+						Already have an account? <span>Login here</span>
+					</Link>
+				</div>
+			)}
 		</div>
 	);
 };
